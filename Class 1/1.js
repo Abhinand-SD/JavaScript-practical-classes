c0d5b19b75e83a7ac6b37f12920d7416b8c9fddf
Abhinand SD <abhinandssd90@gmail.com>
2026-01-20 14:33:44 +0530

    first commit

---
+++ Class 1/1.js
@@ -0,0 +1,12 @@
+console.log("Hello, World!");
+
+console.log(100)
+
+console.log(1 + 2)
+
+const a = 10;
+const b = 20;
+console.log("Total =", a + b);
+console.log("Total =", a - b);
+console.log("Total =", a * b);
+console.log("Total =", a / b);
